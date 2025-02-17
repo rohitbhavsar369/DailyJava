66b8df9eb6fbc99aa507f1e29ded1218eba6033e
rohitbhavsar369 <rohitcbhavsar369@gmail.com>
2025-02-17 22:55:31 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,23 @@
+class name 
+{
+public static void main(String[]args)
+{
+System.out.println("Will print u r name via methods or function ");
+Bhav();
+Rohit();
+Chand();
+}
+public static void Rohit()
+{
+System.out.print("Rohit ");
+}
+public static void Chand()
+{
+System.out.print("Chandrakant ");
+}
+public static void Bhav()
+{
+System.out.print("Bhavsar ");
+
+}
+}
