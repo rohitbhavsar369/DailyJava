30886b871858f34d21f4753764657a700b810e9a
rohitbhavsar369 <rohitcbhavsar369@gmail.com>
2025-02-18 21:22:30 +0530

    Add files via upload

---
+++ opAdd.java
@@ -0,0 +1,22 @@
+class opAdd
+{
+	public static void main(String[]args)
+	{
+		byte res1=33+3;
+		short res2=33+3;
+		int res3=33+3;
+		long res4=33+3;
+		float res5=33+3;
+		double res6=33+3;
+		//boolean res7=33+3;
+
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
+		System.out.println(res4);
+		System.out.println(res5);
+		System.out.println(res6);
+
+
+	}
+}
