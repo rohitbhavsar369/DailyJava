85a436b4494cd27c167779e94b5c8e86ecec0792
rohitbhavsar369 <rohitcbhavsar369@gmail.com>
2025-02-24 17:51:31 +0530

    Add files via upload

---
+++ LogicalOr.java
@@ -0,0 +1,11 @@
+class LogicalOr
+{
+	public static void main(String[] args)
+	{
+	System.out.println(356>569 || 2>0);
+	System.out.println('r'>'o' || 'h'>'i');
+	System.out.println(false || true);
+	System.out.println(false || false);
+
+	}
+}
